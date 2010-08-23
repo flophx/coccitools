b4d8d12f28ac9f5679bffc43ce907dbc993c77a8
flophx <florian.many@gmail.com>
2010-08-23 10:15:45 +0200

    add copyrights

--- lib/python/coccitools/cocci_delete.py
+++ lib/python/coccitools/cocci_delete.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
+
+# This file is part of Coccitools.
+
+# Copyright (C) 2010  Florian MANY
+
+# Coccitools is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Coccitools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with Coccitools.  If not, see <http://www.gnu.org/licenses/>.
+
 import os, sys
 import argparse
 import shutil
